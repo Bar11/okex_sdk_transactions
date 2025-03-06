a8317d9c9d6a57f9d15504b7a86fd8f7642b4bb7
Bar11 <zxg259370@163.com>
2025-03-07 00:07:43 +0800

    Initial commit

---
+++ config/config.py
@@ -0,0 +1,21 @@
+
+
+TEST_FLAG = "1"  # 实盘: 0, 模拟盘: 1
+
+LOGIN_INFO={
+    "APIKey":"",
+    "SecretKey":"",
+    "Passphrase":""
+}
+LOG_INFO = {
+
+}
+
+MYSQL_INFO = {
+    "host":"127.0.0.1",
+    "user":"root",
+    "password":"<PASSWORD>",
+    "db":"okex",
+    "charset":"utf8"
+}
+
