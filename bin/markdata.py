42f8b82ee4813dcc75d204a9d1e6c57c6a4f3ba9
Bar11 <zxg259370@163.com>
2025-03-11 01:05:56 +0800

    交易类和数据获取类重写

---
+++ bin/markdata.py
@@ -0,0 +1,42 @@
+import okx.MarketData as MarketData
+from config.config import TEST_FLAG
+
+flag = "0"  # 实盘:0 , 模拟盘：1
+
+
+
+
+
+
+class MarkData:
+    def __init__(self):
+        self.flag = TEST_FLAG
+        self.marketDataAPI =  MarketData.MarketAPI(flag=flag)
+
+    # 获取所有产品行情信息
+    def get_tickers(self):
+        return self.marketDataAPI.get_tickers(instType="SPOT")
+
+    # 获取产品行情信息
+    def get_ticker(self,instId):
+        return self.marketDataAPI.get_ticker(instId=instId)
+
+    # 获取产品深度
+    def get_orderbook(self,instId):
+        return self.marketDataAPI.get_orderbook(instId=instId)
+
+    # 获取K线数据。K线数据按请求的粒度分组返回，K线数据每个粒度最多可获取最近1,440条。
+    def get_candlesticks(self,instId):
+        return self.marketDataAPI.get_candlesticks(instId=instId)
+
+    # 获取最近几年的历史k线数据(1s k线支持查询最近3个月的数据)
+    '''
+    after 请求此时间戳之前（更旧的数据）的分页内容，传的值为对应接口的ts
+    before 请求此时间戳之后（更新的数据）的分页内容，传的值为对应接口的ts, 单独使用时，会返回最新的数据。
+    bar 时间粒度，默认值1m 如 [1s/1m/3m/5m/15m/30m/1H/2H/4H]
+                        香港时间开盘价k线：[6H/12H/1D/2D/3D/1W/1M/3M]
+                        UTC时间开盘价k线：[6Hutc/12Hutc/1Dutc/2Dutc/3Dutc/1Wutc/1Mutc/3Mutc]
+    limit 分页返回的结果集数量，最大为100，不填默认返回100条
+    '''
+    def get_history_candlesticks(self, instId):
+        return self.marketDataAPI.get_history_candlesticks(instId=instId)
--- bin/transaction.py
+++ bin/transaction.py
@@ -7,19 +7,25 @@ class Transaction:
         self.secretkey = SECRET_INFO["SecretKey"]
         self.passphrase = SECRET_INFO["Passphrase"]
         self.flag = TEST_FLAG
+        self.tradeAPI = Trade.TradeAPI(self.flagey, self.flagetkey, self.flagphrase, False, self.flag)
 
-        tradeAPI = Trade.TradeAPI(self.flagey, self.flagetkey, self.flagphrase, False, self.flag)
+    # 现货模式限价单
+    def place_order(self,order_data):
+        instId = order_data['instId']
+        tdMode = order_data['tdMode']
+        clOrdId = order_data['clOrdId']
+        side = order_data['side']
+        ordType = order_data['ordType']
+        px = order_data['px']
+        sz = order_data['sz']
+        return self.tradeAPI.place_order(instId=instId,
+                                        tdMode=tdMode,
+                                        clOrdId=clOrdId,
+                                        side=side,
+                                        ordType=ordType,
+                                        px=px,
+                                        sz=sz)
 
-        # 现货模式限价单
-        result = tradeAPI.place_order(
-            instId="BTC-USDT",
-            tdMode="cash",
-            clOrdId="b15",
-            side="buy",
-            ordType="limit",
-            px="2.15",
-            sz="2"
-        )
-
-        print(result)
-
+    # 撤单
+    def cancel_order(self,instId,ordId ):
+        return self.tradeAPI.cancel_order(instId=instId,ordId=ordId)
